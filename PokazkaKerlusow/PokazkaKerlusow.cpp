307eb219b50db173ad971c01250157aa7f497fbd
Daniel Zoba <dani@rapaki.de>
2024-07-07 14:42:54 +0200

    UDP reception basically working

--- PokazkaKerlusow/PokazkaKerlusow.cpp
+++ PokazkaKerlusow/PokazkaKerlusow.cpp
@@ -1,6 +1,66 @@
 #include <iostream>
 
+// TODO get rid of this
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <winsock2.h>
+#pragma comment(lib,"ws2_32.lib") 
+#include <ws2tcpip.h>
+
+static WSADATA wsa;
+static SOCKET server_socket;
+static sockaddr_in server, client;
+
+#define PORT 49200
+#define BUFLEN 61
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	// initialise winsock
+	std::cout << "Initialising Winsock...\n";
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		std::cout << "Failed. Error Code: " << WSAGetLastError() << "\n";
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "Initialised.\n";
+
+	// create a socket
+	if ((server_socket = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
+		std::cout << "Could not create socket: " << WSAGetLastError() << "\n";
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "Socket created.\n";
+
+	// prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(PORT);
+
+	// bind
+	if (bind(server_socket, (sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+		std::cout << "Bind failed with error code: " << WSAGetLastError() << "\n";
+		exit(EXIT_FAILURE);
+	}
+	std::cout << "Bind done.\n";
+
+	while (true)
+	{
+		char message[BUFLEN] = {};
+
+		// try to receive some data, this is a blocking call
+		int message_len;
+		int slen = sizeof(sockaddr_in);
+		if ((message_len = recvfrom(server_socket, message, BUFLEN, 0, (sockaddr*)&client, &slen)) == SOCKET_ERROR) {
+			std::cout << "recvfrom() failed with error code: " << WSAGetLastError() << "\n";
+			exit(0);
+		}
+
+		// print details of the client/peer and the data received
+		std::cout << "Received packet from " << inet_ntoa(client.sin_addr) << " " << ntohs(client.sin_port) << "\n";
+		// TODO take care of zero-termination!
+		std::cout << "Data: " << message << "\n";
+	}
+
+	closesocket(server_socket);
+	WSACleanup();
 }
